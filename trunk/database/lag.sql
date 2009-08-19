7f19d3962d7ff910be21c24ad239d6be3af303a7
Maxime.me <Maxime.me@62d8ab44-8a38-11de-b841-6521bc8c149b>
2009-08-19 17:13:21 +0000

    modif sql

--- trunk/database/lag.sql
+++ trunk/database/lag.sql
@@ -3,6 +3,10 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 -- Base de données: `lag`
 --
+DROP DATABASE IF EXISTS `lag`;
+CREATE DATABASE `lag` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci; 
+
+USE `lag`;
 
 -- --------------------------------------------------------
 -- -------------------- SITE ------------------------------
@@ -16,13 +20,13 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 DROP TABLE IF EXISTS `lan`;
 CREATE TABLE IF NOT EXISTS `lag` (
 	`idLan`	int(10)	unsigned NOT NULL AUTO_INCREMENT,
-	`nom` varchar(30) NOT NULL,
-	`adresse` varchar(60) NOT NULL,
+	`nom` varchar(30) CHARACTER SET utf8 NOT NULL,
+	`adresse` varchar(60) CHARACTER SET utf8 NOT NULL,
 	`datedeb` date NOT NULL,
 	`datefin` date NOT NULL,
 	`nbmaxpers` int(10) NOT NULL,
 	`prix` int(10) NOT NULL,
-	`description` longtext,
+	`description` longtext CHARACTER SET utf8,
 	PRIMARY KEY(`idLan`)
 
 )ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE utf8_general_ci AUTO_INCREMENT=1;
@@ -35,10 +39,10 @@ CREATE TABLE IF NOT EXISTS `lag` (
 DROP TABLE IF EXISTS `jeux`;
 CREATE TABLE IF NOT EXISTS `jeux` (
 	`idJeux`	int(10)	unsigned NOT NULL AUTO_INCREMENT,
-	`nom` varchar(30) NOT NULL,
-	`type` varchar(20) NOT NULL,
-	`lien` varchar(60) NOT NULL,
-	`description` longtext,
+	`nom` varchar(30) CHARACTER SET utf8 NOT NULL,
+	`type` varchar(20) CHARACTER SET utf8 NOT NULL,
+	`lien` varchar(60) CHARACTER SET utf8 NOT NULL,
+	`description` longtext CHARACTER SET utf8,
 	PRIMARY KEY(`idJeux`)
 
 )ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE utf8_general_ci AUTO_INCREMENT=1;
@@ -57,7 +61,9 @@ CREATE TABLE IF NOT EXISTS `lanjeux` (
 	`nbmaxjoueur` int(10) NOT NULL,
 	`nbmaxjoueurparteam` int(10) NOT NULL,
 	`ordre` int(10) NOT NULL,
-	PRIMARY KEY(`idLanJeux`)
+	PRIMARY KEY(`idLanJeux`),
+	FOREIGN KEY (`idLan`) REFERENCES `lan`(`idLan`),
+	FOREIGN KEY (`idJeux`) REFERENCES `jeux`(`idJeux`)
 
 )ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE utf8_general_ci AUTO_INCREMENT=1;
 
@@ -69,7 +75,7 @@ CREATE TABLE IF NOT EXISTS `lanjeux` (
 DROP TABLE IF EXISTS `statut`;
 CREATE TABLE IF NOT EXISTS `statut` (
 	`idStatut`	int(10)	unsigned NOT NULL AUTO_INCREMENT,
-	`nom` varchar(30) NOT NULL,
+	`nom` varchar(30) CHARACTER SET utf8 NOT NULL,
 	`ordre` int(10) NOT NULL,
 	PRIMARY KEY(`idStatut`)
 
@@ -84,20 +90,21 @@ DROP TABLE IF EXISTS `compte`;
 CREATE TABLE IF NOT EXISTS `compte` (
 	`idCompte`	int(10)	unsigned NOT NULL AUTO_INCREMENT,
 	`idStatut` int(10) unsigned NOT NULL,
-	`login` varchar(60) NOT NULL,
-	`password` varchar(60) NOT NULL,
-	`nom` varchar(30) NOT NULL,
-	`prenom` varchar(30) NOT NULL,
+	`login` varchar(60) CHARACTER SET utf8 NOT NULL,
+	`password` varchar(60) CHARACTER SET utf8 NOT NULL,
+	`nom` varchar(30) CHARACTER SET utf8 NOT NULL,
+	`prenom` varchar(30) CHARACTER SET utf8 NOT NULL,
 	`datenaissance` date not null,
 	`dateins` date not null,
 	`dateco` date not null,
 	`temps` int(10) not null,
-	`ip` varchar(15) not null,
-	`jeuxprefere` varchar(100),
-	`configpc` longtext,
-	`citationpreferee` varchar(140),
-	`description` longtext,
-	PRIMARY KEY(`idCompte`)
+	`ip` varchar(15) CHARACTER SET utf8 not null,
+	`jeuxprefere` varchar(100) CHARACTER SET utf8,
+	`configpc` longtext CHARACTER SET utf8,
+	`citationpreferee` varchar(140) CHARACTER SET utf8,
+	`description` longtext CHARACTER SET utf8,
+	PRIMARY KEY(`idCompte`),
+	FOREIGN KEY (`idStatut`) REFERENCES `statut`(`idStatut`)
 
 )ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE utf8_general_ci AUTO_INCREMENT=1;
 
@@ -115,7 +122,10 @@ CREATE TABLE IF NOT EXISTS `lanjeuxjoueur` (
 	`dateins` date not null,
 	`paiement` tinyint not null,
 	`validation` bool not null,
-	PRIMARY KEY(`idLanJeuxJoueur`)
+	PRIMARY KEY(`idLanJeuxJoueur`),
+	FOREIGN KEY (`idLan`) REFERENCES `lan`(`idLan`),
+	FOREIGN KEY (`idJeux`) REFERENCES `jeux`(`idJeux`),
+	FOREIGN KEY (`idCompte`) REFERENCES `compte`(`idCompte`)
 
 )ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE utf8_general_ci AUTO_INCREMENT=1;
 
@@ -128,7 +138,7 @@ DROP TABLE IF EXISTS `charte`;
 CREATE TABLE IF NOT EXISTS `charte` (
 	`idCharte`	int(10)	unsigned NOT NULL AUTO_INCREMENT,
 	`idJeux` int(10) unsigned NOT NULL,
-	`nom` varchar(60) NOT NULL,
+	`nom` varchar(60) CHARACTER SET utf8 NOT NULL,
 	`ordre` int(10) NOT NULL,
 	PRIMARY KEY(`idCharte`)
 
@@ -143,10 +153,11 @@ DROP TABLE IF EXISTS `chartearticle`;
 CREATE TABLE IF NOT EXISTS `chartearticle` (
 	`idCharteArticle`	int(10)	unsigned NOT NULL AUTO_INCREMENT,
 	`idCharte` int(10) unsigned NOT NULL,
-	`titre` varchar(60) NOT NULL,
-	`description` longtext,
+	`titre` varchar(60) CHARACTER SET utf8 NOT NULL,
+	`description` longtext CHARACTER SET utf8,
 	`ordre` int(10) NOT NULL,
-	PRIMARY KEY(`idCharteArticle`)
+	PRIMARY KEY(`idCharteArticle`),
+	FOREIGN KEY (`idCharte`) REFERENCES `charte`(`idCharte`)
 
 )ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE utf8_general_ci AUTO_INCREMENT=1;
 
@@ -159,9 +170,9 @@ DROP TABLE IF EXISTS `news`;
 CREATE TABLE IF NOT EXISTS `news` (
 	`idNews`	int(10)	unsigned NOT NULL AUTO_INCREMENT,
 	`idCompte` int(10) unsigned NOT NULL,
-	`titre` varchar(60) NOT NULL,
+	`titre` varchar(60) CHARACTER SET utf8 NOT NULL,
 	`date` date NOT NULL,
-	`description` longtext,
+	`description` longtext CHARACTER SET utf8,
 	PRIMARY KEY(`idNews`)
 
 )ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE utf8_general_ci AUTO_INCREMENT=1;
@@ -177,8 +188,10 @@ CREATE TABLE IF NOT EXISTS `newscom` (
 	`idNews` int(10) unsigned NOT NULL,
 	`idCompte` int(10) unsigned NOT NULL,
 	`date` date NOT NULL,
-	`description` longtext,
-	PRIMARY KEY(`idNewsCom`)
+	`description` longtext CHARACTER SET utf8,
+	PRIMARY KEY(`idNewsCom`),
+	FOREIGN KEY (`idNews`) REFERENCES `news`(`idNews`),
+	FOREIGN KEY (`idCompte`) REFERENCES `compte`(`idCompte`)
 
 )ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE utf8_general_ci AUTO_INCREMENT=1;
 
@@ -190,11 +203,11 @@ CREATE TABLE IF NOT EXISTS `newscom` (
 DROP TABLE IF EXISTS `multimedia`;
 CREATE TABLE IF NOT EXISTS `multimedia` (
 	`idMultimedia`	int(10)	unsigned NOT NULL AUTO_INCREMENT,
-	`titre` varchar(60) NOT NULL,
-	`type` varchar(40) NOT NULL,
+	`titre` varchar(60) CHARACTER SET utf8 NOT NULL,
+	`type` varchar(40) CHARACTER SET utf8 NOT NULL,
 	`date` date NOT NULL,
-	`lien` varchar(120),
-	`description` longtext,
+	`lien` varchar(120) CHARACTER SET utf8,
+	`description` longtext CHARACTER SET utf8,
 	PRIMARY KEY(`idMultimedia`)
 
 )ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE utf8_general_ci AUTO_INCREMENT=1;
@@ -210,8 +223,9 @@ CREATE TABLE IF NOT EXISTS `multimediacom` (
 	`idMultimedia` int(10) unsigned NOT NULL,
 	`idCompte` int(10) unsigned NOT NULL,
 	`date` date NOT NULL,
-	`description` longtext,
-	PRIMARY KEY(`idMultimediaCom`)
+	`description` longtext CHARACTER SET utf8,
+	PRIMARY KEY(`idMultimediaCom`),
+	FOREIGN KEY (`idMultimedia`) REFERENCES `multimedia`(`idMultimedia`)
 
 )ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE utf8_general_ci AUTO_INCREMENT=1;
 
@@ -223,7 +237,7 @@ CREATE TABLE IF NOT EXISTS `multimediacom` (
 DROP TABLE IF EXISTS `fonction`;
 CREATE TABLE IF NOT EXISTS `fonction` (
 	`idFonction`	int(10)	unsigned NOT NULL AUTO_INCREMENT,
-	`nom` varchar(40) NOT NULL,
+	`nom` varchar(40) CHARACTER SET utf8 NOT NULL,
 	`ordre` int(10) NOT NULL,
 	PRIMARY KEY(`idFonction`)
 
@@ -239,7 +253,9 @@ CREATE TABLE IF NOT EXISTS `fonctioncompte` (
 	`idFonctionCompte`	int(10)	unsigned NOT NULL AUTO_INCREMENT,
 	`idFonction` int(10) unsigned NOT NULL,
 	`idCompte` int(10) unsigned NOT NULL,
-	PRIMARY KEY(`idFonctionCompte`)
+	PRIMARY KEY(`idFonctionCompte`),
+	FOREIGN KEY (`idFonction`) REFERENCES `fonction`(`idFonction`),
+	FOREIGN KEY (`idCompte`) REFERENCES `compte`(`idCompte`)
 
 )ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE utf8_general_ci AUTO_INCREMENT=1;
 
@@ -251,8 +267,8 @@ CREATE TABLE IF NOT EXISTS `fonctioncompte` (
 DROP TABLE IF EXISTS `mentionlegale`;
 CREATE TABLE IF NOT EXISTS `mentionlegale` (
 	`idMentionLegale`	int(10)	unsigned NOT NULL AUTO_INCREMENT,
-	`titre` varchar(40) NOT NULL,
-	`description` longtext,
+	`titre` varchar(40) CHARACTER SET utf8 NOT NULL,
+	`description` longtext CHARACTER SET utf8,
 	`ordre` int(10),
 	PRIMARY KEY(`idMentionLegale`)
 
@@ -266,12 +282,12 @@ CREATE TABLE IF NOT EXISTS `mentionlegale` (
 DROP TABLE IF EXISTS `materiel`;
 CREATE TABLE IF NOT EXISTS `materiel` (
 	`idMateriel`	int(10)	unsigned NOT NULL AUTO_INCREMENT,
-	`nom` varchar(40) NOT NULL,
-	`type` varchar(40) NOT NULL,
+	`nom` varchar(40) CHARACTER SET utf8 NOT NULL,
+	`type` varchar(40) CHARACTER SET utf8 NOT NULL,
 	`dateachat` date NOT NULL,
-	`description` longtext,
+	`description` longtext CHARACTER SET utf8,
 	`ordre` int(10),
-	`img` varchar(200),
+	`img` varchar(200) CHARACTER SET utf8,
 	PRIMARY KEY(`idMateriel`)
 
 )ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE utf8_general_ci AUTO_INCREMENT=1;
@@ -283,18 +299,19 @@ CREATE TABLE IF NOT EXISTS `materiel` (
 --
 DROP TABLE IF EXISTS `bug`;
 CREATE TABLE IF NOT EXISTS `bug` (
-  `idBug` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `idCompte` int(10) unsigned NOT NULL,
-  `titre` varchar(60) CHARACTER SET utf8 NOT NULL,
-  `datebug` date NOT NULL,
-  `datedebug` date NOT NULL,
-  `statut` varchar(20) CHARACTER SET utf8 NOT NULL,
-  `priorite` int(11) NOT NULL,
-  `categorie` varchar(30) CHARACTER SET utf8 NOT NULL,
-  `navigateur` varchar(20) CHARACTER SET utf8 NOT NULL,
-  `description` longtext CHARACTER SET utf8 NOT NULL,
-  `noteDebugger` longtext CHARACTER SET utf8 NOT NULL,
-  PRIMARY KEY (`idBug`)
+	`idBug` int(10) unsigned NOT NULL AUTO_INCREMENT,
+	`idCompte` int(10) unsigned NOT NULL,
+	`titre` varchar(60) CHARACTER SET utf8 NOT NULL,
+	`datebug` date NOT NULL,
+	`datedebug` date NOT NULL,
+	`statut` varchar(20) CHARACTER SET utf8 NOT NULL,
+	`priorite` int(11) NOT NULL,
+	`categorie` varchar(30) CHARACTER SET utf8 NOT NULL,
+	`navigateur` varchar(20) CHARACTER SET utf8 NOT NULL,
+	`description` longtext CHARACTER SET utf8 NOT NULL,
+	`noteDebugger` longtext CHARACTER SET utf8 NOT NULL,
+	PRIMARY KEY (`idBug`),
+	FOREIGN KEY (`idCompte`) REFERENCES `compte`(`idCompte`)
 
 )ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE utf8_general_ci AUTO_INCREMENT=1;
 
@@ -306,11 +323,11 @@ CREATE TABLE IF NOT EXISTS `bug` (
 
 DROP TABLE IF EXISTS `config`;
 CREATE TABLE IF NOT EXISTS `config` (
-  `idConfig` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `titre` varchar(60) DEFAULT NULL,
-  `cle` varchar(45) DEFAULT NULL,
-  `valeur` varchar(15) DEFAULT NULL,
-  PRIMARY KEY (`idConfig`)
+    `idConfig` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `titre` varchar(60) CHARACTER SET utf8 DEFAULT NULL,
+    `cle` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
+    `valeur` varchar(15) CHARACTER SET utf8 DEFAULT NULL,
+    PRIMARY KEY (`idConfig`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE utf8_general_ci AUTO_INCREMENT=1 ;
 
 
@@ -326,12 +343,12 @@ CREATE TABLE IF NOT EXISTS `config` (
 
 DROP TABLE IF EXISTS `categorie`;
 CREATE TABLE IF NOT EXISTS `categorie` (
-  `idCategorie` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `titre` varchar(60) NOT NULL,
-  `description` longtext NOT NULL,
-  `visible` tinyint NOT NULL,
-  `ordre` int(10) NOT NULL,
-  PRIMARY KEY (`idCategorie`)
+    `idCategorie` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `titre` varchar(60) CHARACTER SET utf8 NOT NULL,
+    `description` longtext CHARACTER SET utf8 NOT NULL,
+    `visible` tinyint NOT NULL,
+    `ordre` int(10) NOT NULL,
+    PRIMARY KEY (`idCategorie`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE utf8_general_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -342,13 +359,14 @@ CREATE TABLE IF NOT EXISTS `categorie` (
 
 DROP TABLE IF EXISTS `souscategorie`;
 CREATE TABLE IF NOT EXISTS `souscategorie` (
-  `idSousCategorie` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `idCategorie` int(10) unsigned NOT NULL,
-  `titre` varchar(60) NOT NULL,
-  `description` longtext NOT NULL,
-  `visible` tinyint NOT NULL,
-  `ordre` int(10) NOT NULL,
-  PRIMARY KEY (`idSousCategorie`)
+    `idSousCategorie` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `idCategorie` int(10) unsigned NOT NULL,
+    `titre` varchar(60) CHARACTER SET utf8 NOT NULL,
+    `description` longtext CHARACTER SET utf8 NOT NULL,
+    `visible` tinyint NOT NULL,
+    `ordre` int(10) NOT NULL,
+    PRIMARY KEY (`idSousCategorie`),
+	FOREIGN KEY (`idCategorie`) REFERENCES `categorie`(`idCategorie`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE utf8_general_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -359,15 +377,17 @@ CREATE TABLE IF NOT EXISTS `souscategorie` (
 
 DROP TABLE IF EXISTS `sujet`;
 CREATE TABLE IF NOT EXISTS `sujet` (
-  `idSujet` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `idSousCategorie` int(10) unsigned NOT NULL,
-  `idCompte` int(10) unsigned NOT NULL,
-  `titre` varchar(60) NOT NULL,
-  `date` date NOT NULL,
-  `vu` bool NOT NULL,
-  `ip` varchar(15) NOT NULL,
-  `visible` tinyint NOT NULL,
-  PRIMARY KEY (`idSujet`)
+    `idSujet` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `idSousCategorie` int(10) unsigned NOT NULL,
+    `idCompte` int(10) unsigned NOT NULL,
+    `titre` varchar(60) CHARACTER SET utf8 NOT NULL,
+    `date` date NOT NULL,
+    `vu` bool NOT NULL,
+    `ip` varchar(15) CHARACTER SET utf8 NOT NULL,
+    `visible` tinyint NOT NULL,
+    PRIMARY KEY (`idSujet`),
+	FOREIGN KEY (`idSousCategorie`) REFERENCES `souscategorie`(`idSousCategorie`),
+	FOREIGN KEY (`idCompte`) REFERENCES `compte`(`idCompte`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE utf8_general_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -378,13 +398,15 @@ CREATE TABLE IF NOT EXISTS `sujet` (
 
 DROP TABLE IF EXISTS `message`;
 CREATE TABLE IF NOT EXISTS `message` (
-  `idMessage` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `idSujet` int(10) unsigned NOT NULL,
-  `idCompte` int(10) unsigned NOT NULL,
-  `date` date NOT NULL,
-  `description` longtext NOT NULL,
-  `ip` varchar(15) NOT NULL,
-  PRIMARY KEY (`idMessage`)
+    `idMessage` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `idSujet` int(10) unsigned NOT NULL,
+    `idCompte` int(10) unsigned NOT NULL,
+    `date` date NOT NULL,
+    `description` longtext CHARACTER SET utf8 NOT NULL,
+    `ip` varchar(15) CHARACTER SET utf8 NOT NULL,
+    PRIMARY KEY (`idMessage`),
+	FOREIGN KEY (`idSujet`) REFERENCES `sujet`(`idSujet`),
+	FOREIGN KEY (`idCompte`) REFERENCES `compte`(`idCompte`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE utf8_general_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -400,13 +422,13 @@ CREATE TABLE IF NOT EXISTS `message` (
 DROP TABLE IF EXISTS `partenaire`;
 CREATE TABLE IF NOT EXISTS `partenaire` (
   `idPartenaire` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `titre` varchar(60) NOT NULL,
+  `titre` varchar(60) CHARACTER SET utf8 NOT NULL,
   `date` date NOT NULL,
   `nbclick` int(10) NOT NULL,
-  `type` varchar(60) NOT NULL,
-  `lien` varchar(60) NOT NULL,
-  `logo` varchar(200) NOT NULL,
-  `description` longtext NOT NULL,
+  `type` varchar(60) CHARACTER SET utf8 NOT NULL,
+  `lien` varchar(60) CHARACTER SET utf8 NOT NULL,
+  `logo` varchar(200) CHARACTER SET utf8 NOT NULL,
+  `description` longtext CHARACTER SET utf8 NOT NULL,
   `ordre` int(10) NOT NULL,
   PRIMARY KEY (`idPartenaire`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE utf8_general_ci AUTO_INCREMENT=1 ;
@@ -420,8 +442,8 @@ CREATE TABLE IF NOT EXISTS `partenaire` (
 DROP TABLE IF EXISTS `lien`;
 CREATE TABLE IF NOT EXISTS `lien` (
   `idLien` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `titre` varchar(60) NOT NULL,
-  `lien` varchar(60) NOT NULL,
+  `titre` varchar(60) CHARACTER SET utf8 NOT NULL,
+  `lien` varchar(60) CHARACTER SET utf8 NOT NULL,
   `ordre` int(10) NOT NULL,
   PRIMARY KEY (`idLien`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE utf8_general_ci AUTO_INCREMENT=1 ;
@@ -435,10 +457,10 @@ CREATE TABLE IF NOT EXISTS `lien` (
 DROP TABLE IF EXISTS `contact`;
 CREATE TABLE IF NOT EXISTS `contact` (
   `idContact` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `titre` varchar(60) NOT NULL,
-  `mail` varchar(60) NOT NULL,
-  `type` varchar(60) NOT NULL,
+  `titre` varchar(60) CHARACTER SET utf8 NOT NULL,
+  `mail` varchar(60) CHARACTER SET utf8 NOT NULL,
+  `type` varchar(60) CHARACTER SET utf8 NOT NULL,
   `date` date NOT NULL,
-  `description` longtext NOT NULL,
+  `description` longtext CHARACTER SET utf8 NOT NULL,
   PRIMARY KEY (`idContact`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE utf8_general_ci AUTO_INCREMENT=1 ;
