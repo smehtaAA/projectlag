e5de2ce30e3cc73a7c1d8a7ff053cfa0eb324a70
Maxime.me <Maxime.me@62d8ab44-8a38-11de-b841-6521bc8c149b>
2009-08-29 09:58:09 +0000

    ajout des layouts

---
+++ trunk/application/controllers/AccueilController.php
@@ -0,0 +1,62 @@
+<?php
+
+class AccueilController extends Zend_Controller_Action 
+{
+	protected $_model;
+	
+	public function indexAction()
+	{
+		$smarty = Zend_Registry::get('view');
+		
+		$smarty->display('accueil/index.tpl');
+	}
+	
+    public function indexadminAction() 
+    {
+		$smarty = Zend_Registry::get('view');
+		$log = new SessionMnd();
+		if($log->_getTypeConnected('admin')) {
+			$model  = $this->_getModel();
+			$request = $this->getRequest();
+			$smarty->assign('baseurl',$request->getBaseUrl());
+			
+			$smarty->assign('title','Accueil');
+			$smarty->display('accueil/indexAdmin.tpl');
+		} else {
+			$smarty->display('error/errorconnexion.tpl');
+		}  
+    }
+    
+    public function formAction()
+    {
+		$smarty  = Zend_Registry::get('view');
+		$log = new SessionMnd();
+		if($log->_getTypeConnected('admin')) {
+			$request = $this->getRequest();
+			$id      = (int)$request->getParam('id', 0);
+
+		} else {
+			$smarty->display('error/errorconnexion.tpl');
+		}  
+    }
+    
+    protected function _getModel()
+    {
+        if (null === $this->_model) {
+            require_once APPLICATION_PATH . '/models/Accueil.php';
+            $this->_model = new Model_Accueil();
+        }
+        return $this->_model;
+    }
+
+    protected function _getAccueilForm($id)
+    {
+        require_once APPLICATION_PATH . '/forms/Accueil.php';
+        $form = new Form_Accueil();
+		if($id > 0)
+			$form->setAction($this->_helper->url('form/?id='.$id));
+		else
+			$form->setAction($this->_helper->url('form'));
+        return $form;
+    }
+}
