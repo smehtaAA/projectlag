1f5e9ddb42a44cd526fba1c129e87d7fa35ff992
Maxime.me <Maxime.me@62d8ab44-8a38-11de-b841-6521bc8c149b>
2009-09-08 08:02:47 +0000

    correction log - appel action index+"superadmin, admin, joueur"

--- application/controllers/AccueilController.php
+++ application/controllers/AccueilController.php
@@ -11,18 +11,53 @@ class AccueilController extends Zend_Controller_Action
 		$smarty->display('accueil/index.tpl');
 	}
 	
-    public function indexadminAction() 
+    public function indexsuperadminAction() 
     {
 		$smarty = Zend_Registry::get('view');
-		$log = new SessionMnd();
-		if($log->_getTypeConnected('admin')) {
-			$model  = $this->_getModel();
+		$log = new SessionLAG();
+		if($log->_getTypeConnected('superadmin')) {
+			//$model  = $this->_getModel();
 			$request = $this->getRequest();
 			$smarty->assign('baseurl',$request->getBaseUrl());
 			
 			$smarty->assign('title','Accueil');
 			$smarty->display('accueil/indexAdmin.tpl');
 		} else {
+			$smarty->assign('message', 'Erreur Connexion');
+			$smarty->display('error/errorconnexion.tpl');
+		}  
+    }
+	
+	public function indexadminAction() 
+    {
+		$smarty = Zend_Registry::get('view');
+		$log = new SessionLAG();
+		if($log->_getTypeConnected('admin')) {
+			//$model  = $this->_getModel();
+			$request = $this->getRequest();
+			$smarty->assign('baseurl',$request->getBaseUrl());
+			
+			$smarty->assign('title','Accueil');
+			$smarty->display('accueil/indexAdmin.tpl');
+		} else {
+			$smarty->assign('message', 'Erreur Connexion');
+			$smarty->display('error/errorconnexion.tpl');
+		}  
+    }
+	
+	public function indexjoueurAction() 
+    {
+		$smarty = Zend_Registry::get('view');
+		$log = new SessionLAG();
+		if($log->_getTypeConnected('joueur')) {
+			//$model  = $this->_getModel();
+			$request = $this->getRequest();
+			$smarty->assign('baseurl',$request->getBaseUrl());
+			
+			$smarty->assign('title','Accueil');
+			$smarty->display('accueil/indexAdmin.tpl');
+		} else {
+			$smarty->assign('message', 'Erreur Connexion');
 			$smarty->display('error/errorconnexion.tpl');
 		}  
     }
