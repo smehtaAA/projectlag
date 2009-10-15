164d5485962e14a90adfc9606cb7dc8097a370cc
Maxime.me <Maxime.me@62d8ab44-8a38-11de-b841-6521bc8c149b>
2009-10-15 21:05:42 +0000

    modification resize image lors creation compte

--- trunk/application/controllers/CompteController.php
+++ trunk/application/controllers/CompteController.php
@@ -104,8 +104,9 @@ class CompteController extends Zend_Controller_Action
 							require_once '../library/My/PhpThumb/ThumbLib.inc.php'; 
 							$thumb = PhpThumbFactory::create('../public/images/comptes/tmp/'.$ancien_nom);
 							$thumb->resize(100, 100)->save('../public/images/comptes/'.$dataform["img"]);
-							if(file_exists('../public/images/comptes/'.$ancien_nom))
-								unlink('../public/images/comptes/'.$ancien_nom);
+							$thumb->resize(25, 25)->save('../public/images/comptes/thumb/'.$dataform["img"]);
+							if(file_exists('../public/images/comptes/tmp/'.$ancien_nom))
+								unlink('../public/images/comptes/tmp/'.$ancien_nom);
 						}
 						
 						return $this->_redirect($this->redirection);
--- trunk/application/controllers/InscriptionController.php
+++ trunk/application/controllers/InscriptionController.php
@@ -223,8 +223,8 @@ class InscriptionController extends Zend_Controller_Action
 					{
 						require_once '../library/My/PhpThumb/ThumbLib.inc.php'; 
 						$thumb = PhpThumbFactory::create('../public/images/comptes/tmp/'.$ancien_nom);
-						$thumb->resize(100, 100)->save('../public/images/comptes/'.$dataform["img"].'_'.$new_id);
-						$thumb->resize(25, 25)->save('../public/images/comptes/thumb/'.$dataform["img"].'_'.$new_id);
+							$thumb->resize(100, 100)->save('../public/images/comptes/'.$dataform["img"]);
+							$thumb->resize(25, 25)->save('../public/images/comptes/thumb/'.$dataform["img"]);
 						if(file_exists('../public/images/comptes/tmp/'.$ancien_nom))
 							unlink('../public/images/comptes/tmp/'.$ancien_nom);
 					}
