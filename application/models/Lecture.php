2684af4bd93e2c14857dc254ae0ce29c264f15b6
maxime.me <maxime.me@62d8ab44-8a38-11de-b841-6521bc8c149b>
2009-10-16 16:50:47 +0000

    ajout gestion lecture des sujets

---
+++ application/models/Lecture.php
@@ -0,0 +1,62 @@
+<?php
+
+class Model_Lecture
+{
+    protected $_table;
+
+    public function getTable()
+    {
+        if (null === $this->_table) {
+            require_once APPLICATION_PATH . '/models/DbTable/Lecture.php';
+            $this->_table = new Model_DbTable_Lecture;
+        }
+        return $this->_table;
+    }
+
+    public function save($id,array $data)
+    {	
+        $table  = $this->getTable();
+        $fields = $table->info(Zend_Db_Table_Abstract::COLS);
+        foreach ($data as $field => $value) {
+            if (!in_array($field, $fields)) {
+                unset($data[$field]);
+            }
+        }
+		if($id > 0) {
+			$where = $table->getAdapter()->quoteInto('idGrade = ?', $id);
+			return $table->update($data,$where);
+		}
+		else
+			return $table->insert($data);
+    }
+
+    public function fetchEntries()
+    {
+        return $this->getTable()->fetchAll('1')->toArray();
+    }
+	
+	public function fetchEntriesByCompteAndSujet($idCompte, $idSujet)
+    {
+		$table = $this->getTable();
+		$select = $table->select()->where('idCompte = ?', $idCompte)->where('idSujet = ?', $idSujet);
+		$temp = $table->fetchRow($select);
+		if(isset($temp))
+			return 1;
+		else
+			return 0;
+    }
+    
+    public function deleteBySujet($idSujet)
+    {	
+		$table  = $this->getTable();
+		$where = $table->getAdapter()->quoteInto('idSujet = ?', $idSujet);
+		return $table->delete($where);
+    }
+		
+	public function deleteByCompte($idCompte)
+    {	
+		$table  = $this->getTable();
+		$where = $table->getAdapter()->quoteInto('idCompte = ?', $idCompte);
+		return $table->delete($where);
+    }
+}
