29cb6dc5630a298e20775dee9f2a29ef0faed1ab
Maxime.me <Maxime.me@62d8ab44-8a38-11de-b841-6521bc8c149b>
2010-02-18 23:50:19 +0000

    ajout du form oublimdp

---
+++ trunk/application/forms/Oublimdp.php
@@ -0,0 +1,22 @@
+<?php
+
+class Form_OubliMdp extends Zend_Form
+{
+    public function init()
+    {
+        $this->setMethod('post');
+		$this->setAttrib('enctype', Zend_Form::ENCTYPE_MULTIPART);
+
+	$this->addElement('text', 'email', array(
+            'label'      => 'Email : ',
+            'required'   => true,
+            'validators' => array(
+                 array('validator' => 'StringLength', 'options' => array(2, 60), 'EmailAddress')
+            )
+        ));	
+
+        $this->addElement('submit', 'submit', array(
+            'label'    => 'Envoyer',
+        ));
+    }
+}
